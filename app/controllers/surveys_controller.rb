ef48abb3f7378450b2fe02e699cb7956f25e2b51
Claire Samuels <clairemation@gmail.com>
2016-02-20 22:26:19 -0500

    Debug token verification system

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,20 +1,19 @@
-get '/surveys/:survey_id' do
-  @survey = Survey.find(params[:survey_id])
-  if logged_in?
-    if @survey.user_answers.any?{|user_answer| user_answer.user_id == current_user.id}
-      erb :'/taken-already'
-    else
-      @questions = @survey.questions
-      erb :'surveys/show'
-    end
-  else
-    erb :'/taken-already'
-  end
-end
+# get '/surveys/:survey_id' do
+#   @survey = Survey.find(params[:survey_id])
+#   if logged_in?
+#     if @survey.user_answers.any?{|user_answer| user_answer.user_id == current_user.id}
+#       erb :'/taken-already'
+#     else
+#       @questions = @survey.questions
+#       erb :'surveys/show'
+#     end
+#   else
+#     erb :'/taken-already'
+#   end
+# end
 
 get '/surveys/:survey_id/tokens' do
   @survey = Survey.find(params[:survey_id])
-  # token.generate_key
   @tokens = @survey.tokens
   if @survey.user.id == current_user.id
     erb :token_list
@@ -24,26 +23,14 @@ get '/surveys/:survey_id/tokens' do
 end
 
 post '/surveys/:survey_id/tokens' do
-  survey = Survey.find(params[:survey_id])
-  if logged_in? && survey.user_id == current_user.id
-    token = Token.create(survey_id: survey.id)
-    @tokens = survey.tokens
-    erb :token_list
+  @survey = Survey.find(params[:survey_id])
+  if logged_in? && @survey.user_id == current_user.id
+    token = Token.create(survey_id: @survey.id)
+    @tokens = @survey.tokens
+    redirect "/surveys/#{@survey.id}/tokens"
   end
-    redirect "/users/#{survey.user_id}"
+    redirect "/users/#{@survey.user_id}"
 end
-
-get '/surveys/:survey_id/:key' do
-  @url = params[:key]
-  token = Token.find_by(url: @url, survey_id: params[:survey_id])
-  if token
-    token.destroy
-    redirect "/surveys/#{token.survey_id}"
-  else
-    erb :"/taken-already"
-  end
-end
-
 
 get '/surveys/:survey_id/results' do
   @survey = Survey.find(params[:survey_id])
@@ -55,10 +42,32 @@ get '/surveys/:survey_id/results' do
   end
 end
 
-post '/surveys/:survey_id' do
-  params[:input].each_value do |answer|
-    UserAnswer.create(user_id: current_user.id, answer_id: answer)
+get '/surveys/:survey_id/:key' do
+  @url = params[:key]
+  token = Token.find_by(url: @url, survey_id: params[:survey_id])
+  if token
+    @survey = Survey.find(params[:survey_id])
+    @questions = @survey.questions
+    erb :'surveys/show'
+    # token.destroy
+    # redirect "/surveys/#{token.survey_id}"
+  else
+    erb :"/surveys/taken-already"
   end
-  redirect "/"
+end
+
+post '/surveys/:survey_id/:key' do
+  url = params[:key]
+  survey = Survey.find(params[:survey_id])
+  token = Token.find_by(url: url, survey_id: params[:survey_id])
+  if token
+    params[:input].each_value do |answer|
+      UserAnswer.create(user_id: current_user.id, answer_id: answer)
+    end
+    token.destroy
+    erb :"/surveys/thankyou"
+  else
+    erb :"/taken-already"
+  end
 end
 
